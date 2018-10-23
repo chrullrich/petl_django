340f3e05451ecb81865766ff2726f6a53d12f0a4
Christian Ullrich <chris@chrullrich.net>
2018-10-23 10:51:41 +0200

    Various improvements.
    - Python 3-ified
    - Only load all existing model instances if needed for updating
    - Only update if requested, not always
    - Use f-strings instead of str.format()
    - Assume setuptools is installed

--- petl_django/django_view.py
+++ petl_django/django_view.py
@@ -1,17 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function, division
-
-
-# standard library dependencies
 import logging
-# from petl.compat import next, text_type, string_types
 
-
-# internal dependencies
-# from petl.errors import ArgumentError
 from petl.util.base import Table
 
-# external dependencies
 from django.db.models.query import QuerySet
 from django.db.models import Model
 from django.conf import settings
@@ -29,7 +19,8 @@ warning = logger.warning
 
 def fromdjango(model, queryset, fields=None, *args, **kwargs):
     assert type(queryset) == QuerySet, 'Must be supplied a Django QuerySet'
-    assert issubclass(model, Model), 'Must be supplied a valid Django model class'
+    assert issubclass(model, Model), \
+        'Must be supplied a valid Django model class'
 
     return DjangoView(model, queryset, fields, *args, **kwargs)
 
@@ -64,26 +55,33 @@ def _get_model_field_names(model):
     return [f.name for f in model._meta.fields]
 
 
-def todjango(table, model, update=True, create=True, use_bulk_create=True, *args, **kwargs):
+def todjango(table, model, update=True, create=True, use_bulk_create=True,
+             *args, **kwargs):
     '''
     Given a table with appropriate headings create Django models.
     '''
-    assert issubclass(model, Model), 'Must be supplied a valid Django model class'
+    assert issubclass(model, Model), \
+        'Must be supplied a valid Django model class'
+
     table_iterator = iter(table)
     table_headers = table_iterator.next()
 
     model_pk_field_name = model._meta.pk.name
     model_field_names = _get_model_field_names(model)
-    model_name = model.__name__
-
-    existing_models = _get_django_objects(model)
-    existing_model_map = dict([(m.pk, m) for m in existing_models])
+    model_name = model._meta.label
 
     if update:
-        # if we are going to update existing models we need to have a table field that
-        # corresponds to the model 'pk' field.
-        assert model_pk_field_name in set(model_field_names), 'To be able to update existing models the data must have a field corresponding to the Django Primary Key field {}'.format(model_pk_field_name)
-        # existing_model_pks = model.objects.all().values_list('pk', flat=True) # TODO: this could be a performance bottleneck if lots of models are being updated
+        # if we are going to update existing models we need to have
+        # a table field that corresponds to the model's 'pk' field.
+        assert model_pk_field_name in set(model_field_names), \
+            'To update existing models the data must include the ' \
+            f'Django primary key field {model_pk_field_name}'
+
+        # Load all existing instances.
+        existing_models = _get_django_objects(model)
+        existing_model_map = dict([(m.pk, m) for m in existing_models])
+    else:
+        existing_model_map = dict()
 
     updated_model_count = 0
     unsaved_models = []
@@ -92,18 +90,19 @@ def todjango(table, model, update=True, create=True, use_bulk_create=True, *args
         value_map = dict(zip(table_headers, row))
         pk = value_map.get(model_pk_field_name, None)
 
-        try:
+        if (update and pk in existing_model_map):
             django_object = existing_model_map[pk]
             if _will_model_change(value_map, django_object):
                 _apply_value_map(value_map, django_object)
                 try:
                     django_object.save()
                 except Exception as e:
-                    # Add the data that cause the exception to the exception as reraise
+                    # Add the data that caused the exception
+                    # to the exception and reraise
                     e.petl_data = value_map
                     raise e
                 updated_model_count += 1
-        except KeyError:
+        else:
             django_object = model(**value_map)
             if use_bulk_create:
                 unsaved_models.append(django_object)
@@ -113,15 +112,13 @@ def todjango(table, model, update=True, create=True, use_bulk_create=True, *args
                 except Exception as e:
                     e.petl_data = value_map
                     raise e
-    logger.debug('Bulk creating unsaved {}'.format(model_name))
+
+    logger.debug(f'Bulk creating unsaved {model_name}')
     if use_bulk_create:
         _chunked_bulk_create(model, unsaved_models)
 
-    msg = 'Updated {} existing {}'.format(updated_model_count, model_name)
-    logger.info(msg)
-
-    msg = 'Created {} new {}'.format(len(unsaved_models), model_name)
-    logger.info(msg)
+    logger.info(f'Updated {updated_model_count} existing {model_name}')
+    logger.info(f'Created {len(unsaved_models)} new {model_name}')
 
 
 def _get_django_objects(model):
@@ -129,20 +126,30 @@ def _get_django_objects(model):
     Given a Django model class get all of the current records that match.
     This is better than django's bulk methods and has no upper limit.
     '''
-    model_name = model.__class__.__name__
+    model_name = model._meta.label
     model_objects = [i for i in model.objects.all()]
-    logger.debug('Found {} {} objects in DB'.format(len(model_objects), model_name))
+    logger.debug(f'Found {len(model_objects)} {model_name} objects')
     return model_objects
+
+
+def _get_setting(name):
+    '''
+    Return a value from settings.
+
+    If it is not in Django's settings, look in the local defaults.
+    '''
+    return getattr(settings, name, DEFAULTS[name])
 
 
 def _chunked_bulk_create(django_model_object, unsaved_models, chunk_size=None):
     '''Create new models using bulk_create in batches of `chunk_size`.
     This is designed to overcome a query size limitation in some databases'''
     if chunk_size is None:
-        chunk_size = getattr(settings, 'BULK_CREATE_CHUNK_SIZE', 50)
+        chunk_size = _get_setting('BULK_CREATE_CHUNK_SIZE')
     for i in range(0, len(unsaved_models), chunk_size):
         try:
-            django_model_object.objects.bulk_create(unsaved_models[i:i + chunk_size])
+            django_model_object.objects.bulk_create(
+                unsaved_models[i:i + chunk_size])
         except Exception as e:
             chunk_data = unsaved_models[i:i + chunk_size]
             e.petl_chunk_data = chunk_data
@@ -163,7 +170,9 @@ def _apply_value_map(value_map, django_model_instance):
         try:
             setattr(django_model_instance, k, v)
         except AttributeError:
-            raise UnableToApplyValueMapError('User model {} does not have attribute {}'.format(django_model_instance.__class__.__name__, k))
+            model_name = django_model_instance.__class__._meta.model_name
+            raise UnableToApplyValueMapError(
+                f'Field {k} not present in model {model_name}')
     return django_model_instance
 
 
--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 from petl_django import __version__ as version
 
 long_description = 'Please see the documentation at the `project page <https://github.com/PGower/petl_django>`_ .'
-
-packages = [
-    'petl_django',
-]
 
 package_data = {
     '': ['LICENSE', 'README.md'],
@@ -20,14 +13,13 @@ package_data = {
 setup(
     name='petl_django',
     version=version,
-    description='A petl view class for django model data.',
+    description='A petl view class for Django model data.',
     long_description=long_description,
     author='Paul Gower',
     author_email='p.gower@gmail.com',
     url='https://github.com/PGower/petl_django',
     download_url='https://github.com/PGower/petl_django/releases',
-    package_dir={'petl_django': 'petl_django'},
-    packages=packages,
+    packages=find_packages(),
     package_data=package_data,
     license='MIT',
     classifiers=[
@@ -42,5 +34,5 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords=['petl', 'django'],
-    install_requires=['petl', 'django'],
+    install_requires=['petl', 'Django'],
 )
